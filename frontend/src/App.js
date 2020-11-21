b9766004496681d2ba33f654ab7fa1e820e75c8a
Kyle Barthelmes <kbart2401@gmail.com>
2020-11-20 19:45:25 -0500

    signup functionality working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage/index';
+import SignupFormPage from './components/SignupFormPage/index';
 import { useDispatch, useSelector } from 'react-redux';
 import * as sessionActions from './store/session';
 
 function App() {
   const state = useSelector((state) => state.session.user)
-  // console.log('look here', state.email);
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
@@ -14,16 +14,16 @@ function App() {
   }, [dispatch]);
 
   return isLoaded && (
-    <>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' render={() => <h1>Hello there {state.username}!</h1>} />
           <Route path='/login'>
             <LoginFormPage />
           </Route>
+          <Route path='/signup'>
+            <SignupFormPage />
+          </Route>
         </Switch>
       </BrowserRouter>
-    </>
   );
 }
 
---
+++ frontend/src/components/SignupFormPage/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import {Redirect} from 'react-router-dom';
+import './SignupForm.css';
+
+
+const SignupFormPage = () => {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user)
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [errors, setErrors] = useState([])
+
+  if (sessionUser) return <Redirect to="/" />;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(password === confirmPassword) {
+      setErrors([]);
+      return dispatch(sessionActions.signUpUser({
+        username, email, 
+        password})).catch(res => {
+          if(res.data && res.data.errors) setErrors(res.data.errors);
+        })
+    }
+    return setErrors([`Confirm Password field must be the same as
+     the Password field`])
+  }
+
+  return (
+    <form>
+      <ul>
+        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+      </ul>
+      <label>Username</label>
+      <input required name='username' type='text' value={username}
+      onChange={e => setUsername(e.target.value)}/>
+      <label>Email</label>
+      <input required name='email' type='email' value={email}
+      onChange={e => setEmail(e.target.value)}/>
+      <label>Password</label>
+      <input required name='password' type='password' value={password}
+      onChange={e => setPassword(e.target.value)}/>
+      <label>Confirm Password</label>
+      <input required name='confirmPassword' type='password' value={confirmPassword}
+      onChange={e => setConfirmPassword(e.target.value)}/>
+      <button type='submit'>Sign Up</button>
+    </form>
+  )
+}
+
+export default SignupFormPage;

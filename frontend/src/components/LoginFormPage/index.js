820bef1fb0015bff0ff915b6a7a74bf9ca65c4f1
Kyle B <Kbart2401@gmail.com>
2020-11-21 22:47:44 -0500

    stylized signup page

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -4,13 +4,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, NavLink } from 'react-router-dom';
 import './loginFormPage.css';
 
+
 const LoginFormPage = () => {
   const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user)
   const [credentials, setCredential] = useState('')
   const [password, setPassword] = useState('')
   const [errors, setErrors] = useState([])
-  
+
   if (sessionUser) return <Redirect to='/' />
 
   const handleSubmit = e => {
@@ -21,24 +22,26 @@ const LoginFormPage = () => {
         if (res.data && res.data.errors) setErrors(res.data.errors);
       })
   }
-  
+
   return (
-    <div className='login-container'>
-      <h1>Log in to your account</h1>
-    <form onSubmit={handleSubmit} >
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <input className='login-input' required id='username' name='username'
-        type='text' value={credentials} placeholder='Username'
-        onChange={e => setCredential(e.target.value)} />
-      <input className='login-input' required id='password' name='password'
-        type='password' placeholder='Password'
-        value={password} onChange={e => setPassword(e.target.value)} />
-      <input className='login-submit' type='submit' value='Log in' />
-    </form>
-      <NavLink className='signup-link' to='/signup'>
-        Create a username</NavLink>
+    <div className='login-body'>
+      <div className='login-container'>
+        <h1>Log in to your account</h1>
+        <form onSubmit={handleSubmit} >
+          <ul>
+            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+          </ul>
+          <input className='login-input' required id='username' name='username'
+            type='text' value={credentials} placeholder='Username'
+            onChange={e => setCredential(e.target.value)} />
+          <input className='login-input' required id='password' name='password'
+            type='password' placeholder='Password'
+            value={password} onChange={e => setPassword(e.target.value)} />
+          <input className='login-submit' type='submit' value='Log in' />
+        </form>
+        <NavLink className='signup-link' to='/signup'>
+          Create a username</NavLink>
+      </div>
     </div>
   )
 }
--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import './SignupForm.css';
 
 
@@ -18,37 +18,47 @@ const SignupFormPage = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    if(password === confirmPassword) {
+    if (password === confirmPassword) {
       setErrors([]);
       return dispatch(sessionActions.signUpUser({
-        username, email, 
-        password})).catch(res => {
-          if(res.data && res.data.errors) setErrors(res.data.errors);
-        })
+        username, email,
+        password
+      })).catch(res => {
+        if (res.data && res.data.errors) setErrors(res.data.errors);
+      })
     }
     return setErrors([`Confirm Password field must be the same as
      the Password field`])
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <label>Username</label>
-      <input required name='username' type='text' value={username}
-      onChange={e => setUsername(e.target.value)}/>
-      <label>Email</label>
-      <input required name='email' type='email' value={email}
-      onChange={e => setEmail(e.target.value)}/>
-      <label>Password</label>
-      <input required name='password' type='password' value={password}
-      onChange={e => setPassword(e.target.value)}/>
-      <label>Confirm Password</label>
-      <input required name='confirmPassword' type='password' value={confirmPassword}
-      onChange={e => setConfirmPassword(e.target.value)}/>
-      <button type='submit'>Sign Up</button>
-    </form>
+    <>
+      <div className='signup-page-container'>
+        <div className='signup-page-sidebar'>
+          <h2>Register for online access</h2>
+        </div>
+        <div className='signup-page-form'>
+          <form onSubmit={handleSubmit}>
+            <ul>
+              {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+            </ul>
+            <label>USERNAME</label>
+            <input required name='username' type='text' value={username}
+              onChange={e => setUsername(e.target.value)} />
+            <label>EMAIL</label>
+            <input required name='email' type='email' value={email}
+              onChange={e => setEmail(e.target.value)} />
+            <label>PASSWORD</label>
+            <input required name='password' type='password' value={password}
+              onChange={e => setPassword(e.target.value)} />
+            <label>CONFIRM PASSWORD</label>
+            <input required name='confirmPassword' type='password' value={confirmPassword}
+              onChange={e => setConfirmPassword(e.target.value)} />
+            <button type='submit'>Sign Up</button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 

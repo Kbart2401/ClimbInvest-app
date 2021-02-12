022db627c8c81ee3d2417fe593c4282af30912bc
Kyle Barthelmes <kbart2401@gmail.com>
2021-02-12 16:23:24 -0500

    got rid of unnecessary api calls with one batch call instead and simplified redux store

--- frontend/src/store/stockSearch.js
+++ frontend/src/store/stockSearch.js
@@ -2,20 +2,12 @@ import { fetch } from './csrf';
 
 //action types
 const SET_STOCK = 'SET_STOCK';
-const SET_COMPANY = 'SET_COMPANY';
 const REMOVE_COMPANY = 'REMOVE_COMPANY'
 
 //action creators
 const setStock = (stock) => {
   return {
     type: SET_STOCK,
-    payload: stock
-  }
-}
-
-const setCompany = (stock) => {
-  return {
-    type: SET_COMPANY,
     payload: stock
   }
 }
@@ -46,36 +38,18 @@ export const setStockData = (stock) => async (dispatch) => {
   }
 }
 
-//Search company info thunk
-export const setCompanyInfo = (company) => async (dispatch) => {
-  const companyInfo = await fetch('/api/search/company_data', {
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      company
-    })
-  })
-  dispatch(setCompany(companyInfo.data))
-}
-
 
 /***********Reducer**********/
-export const stockSearchReducer = (state = { stock: null, company: null }, action) => {
+export const stockSearchReducer = (state = { stock: null }, action) => {
 
   switch (action.type) {
     case SET_STOCK:
       return {
         ...state, stock: action.payload
       }
-    case SET_COMPANY:
-      return {
-        ...state, company: action.payload
-      }
     case REMOVE_COMPANY:
       return {
-        ...state, stock: action.payload, company: action.payload
+        ...state, stock: action.payload
       }
     default:
       return state
